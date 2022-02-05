a3fd258e117c8b08cd9ba063300c1a11e34d6ff5
Juju <kosyuska@yandex.ru>
2022-02-05 06:37:12 +0300

    Add project files.

---
+++ Trushkova_HW_18/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task2;
+
+Console.WriteLine("Hello, Teacher!");
+
+Train[] trains = new Train[4];
+
+for (int i = 0; i < trains.Length; i++)
+{
+    trains[i] = Train.MakeTrain(i);
+}
+
+trains = Train.SortingNumTrain(trains);
+Console.WriteLine("\nЗагруженные в базу поезда:");
+for (int i = 0; i < trains.Length; i++)
+{    
+    trains[i].Show();
+}
+
+Console.WriteLine("\nВведите номер поезда для поиска по загруженной базе:");
+int numberTrain = Convert.ToInt32(Console.ReadLine());
+
+int? hasTrainIndex = Train.HasTrain(numberTrain, trains);
+
+if (hasTrainIndex.HasValue)
+    trains[hasTrainIndex.Value].Show();
+else
+    Console.WriteLine("Такого поезда нет");
---
+++ Trushkova_HW_18/Train.cs
@@ -0,0 +1,97 @@
+﻿namespace Task2
+{
+    struct Train
+    {
+        private string _nameStation;
+        private int _numberTrain;
+        private int _time;
+
+        public Train(string name, int number, int time)
+        {
+            _nameStation = name;
+            _numberTrain = number;
+            _time = time;
+        }
+
+        public string NameStation => _nameStation;
+
+        public int NumberTrain => _numberTrain;
+
+        public int Time => _time;
+
+        public void Show()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"Поезд {_numberTrain} отправляется со станции {_nameStation} в {_time} часов";
+        }
+
+        public static Train MakeTrain(int i)
+        {
+            int numberTrain = ReadFromConsole($"номер {i + 1}-го поезда");
+            int time = ReadFromConsole($"время прибытия {i + 1}-го поезда");
+            Console.WriteLine($"Введите название станции для {i + 1}-го поезда:");
+            string? nameStation = Console.ReadLine();
+            if (nameStation != null)
+                return new Train(nameStation, numberTrain, time);
+            else
+            {
+                Console.WriteLine("Имя станции не задано");
+                return new Train();
+            }
+        }
+
+        public static int ReadFromConsole(string name)
+        {
+            int parametr;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine($"Введите {name}:");
+                    parametr = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+            return parametr;
+        }
+
+        public static int? HasTrain(int numberTrain, Train[] trains)
+        {
+            int? hasTrainIndex = null;
+
+            for (int i = 0; i < trains.Length; i++)
+            {
+                if (trains[i].NumberTrain == numberTrain)
+                {
+                    hasTrainIndex = i;
+                    break;
+                }
+            }
+            return hasTrainIndex;
+        }
+
+        public static Train[] SortingNumTrain(Train[] trains)
+        {            
+            for (int i = 1; i < trains.Length; i++)
+            {
+                Train trainDop = trains[i];
+                int j = i;
+                while (j > 0 && trains[j - 1].NumberTrain > trainDop.NumberTrain)
+                {
+                    trains[j] = trains[j - 1];
+                    --j;
+                }
+                trains[j] = trainDop;
+            }
+            return trains;
+        }
+    }
+}
