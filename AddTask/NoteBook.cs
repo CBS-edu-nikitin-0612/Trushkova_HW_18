ecf9b44d3d2dc88b7eb9f239313b2fa05a35b6c2
Juju <kosyuska@yandex.ru>
2022-02-05 06:53:46 +0300

    feat: add addTask

---
+++ AddTask/NoteBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+    struct NoteBook
+    {
+        private string _producer;
+        private int _model;
+        private int _price;
+
+        public NoteBook(string name, int number, int time)
+        {
+            _producer = name;
+            _model = number;
+            _price = time;
+        }
+
+        public string Producer => _producer;
+
+        public int Model => _model;
+
+        public int Price => _price;
+
+        public void Show()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"Модель планшета {_model} производится {_producer} по цене {_price} рублей";
+        }
+    }
+}
---
+++ AddTask/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AddTask;
+
+Console.WriteLine("Hello, Teacher!");
+
+NoteBook[] notebooks = new NoteBook[4];
+
+for (int i = 0; i < notebooks.Length; i++)
+{
+    notebooks[i] = new NoteBook($"producer{i + 1}", (i + 1) * 10, (i + 1) * 1000);
+}
+
+
+Console.WriteLine("\nЗагруженные в базу модели планшетов:");
+for (int i = 0; i < notebooks.Length; i++)
+{
+    notebooks[i].Show();
+}
